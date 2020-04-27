304ddeb73a2b81160eced28138ba61b322444116
Hannna <hmw41@miami.edu>
2020-04-26 21:28:13 -0400

    Initial Commit

---
+++ mood/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mood
+//
+//  Created by Hanna Wollin on 4/26/20.
+//  Copyright © 2020 Hanna Wollin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
