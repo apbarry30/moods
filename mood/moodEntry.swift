4061174e815d1ffadce049f838da67ab6379873e
apbarry30 <apbarry30@miami.edu>
2020-04-29 12:31:38 -0400

    added mood entry.swift

---
+++ mood/moodEntry.swift
@@ -0,0 +1,21 @@
+//
+//  moodEntry.swift
+//  mood
+//
+//  Created by Amanda Barry on 4/27/20.
+//  Copyright © 2020 Hanna Wollin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class moodEntry: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+  @IBOutlet var reminders: UITextField!
+
+}
