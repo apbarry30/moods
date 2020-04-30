b9c35849e23d7fb81cf4692f44dbdefdf8696d1d
apbarry30 <apbarry30@miami.edu>
2020-04-30 17:53:36 -0400

    added slider and picker code

--- mood/moodEntry.swift
+++ mood/moodEntry.swift
@@ -10,14 +10,64 @@ import Foundation
 import UIKit
 
 
-class moodEntry: UIViewController {
-    var happyface2
-       var happyface1
+class moodEntry: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+     
+      @IBOutlet var slider:UISlider!
+      var currentValue: Int = 50
+      @IBOutlet var reminders: UITextField!
+        var happyface2: UIImage!
+        var happyface1: UIImage!
+    //picker variables
+        var weekArray:[String] = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        var curDay: String?
+    //slider variables
+    @IBOutlet var outputMoodScale: UILabel!
+
+
+    
+//protocol for picker view;
+      func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+             if (component == 0){
+                 curDay = weekArray[row]
+                print(curDay!)
+                //user just selected their day of the week
+             }
+         }
+         func numberOfComponents(in pickerView: UIPickerView) -> Int {
+             return 1
+         }
+         
+         //protocol for data source
+         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            
+                 return weekArray.count
+             
+         }
+         
+         //protocol for delegate
+         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return String(weekArray[row])
+            
+         }
+    
+    
+    
+    //slidercode
+    @IBAction func sliderMoved(slider:UISlider){
+          
+          currentValue = lroundf(slider.value)
+          print("the value of the slider is now: \(slider.value)")
+            outputMoodScale.text = String(currentValue)
+      }
+    
+      //reset when done
+    @IBAction func reset(){
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-   
-  @IBOutlet var reminders: UITextField!
 
 }
