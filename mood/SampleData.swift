4eff59edd60d751590a67bf038ccc4b1a8a966e4
Hannna <hmw41@miami.edu>
2020-05-02 00:05:41 -0400

    finished inspiration page and something for opening

--- mood/SampleData.swift
+++ mood/SampleData.swift
@@ -12,8 +12,14 @@ final class SampleData{
         return[
             Article(image: "moodballs.jpg", title: "Strategies to Pull Youself Out of a Bad Mood"),
             Article(image: "fall.png", title: "Why Do We Fall - Motivational Video"),
+            Article(image: "anxiety.png", title: "9 Tips for Anxiety Relief & Management"),
+            Article(image: "nyt3.png", title: "How to Be Happy"),
+          
+            Article(image: "covid.png", title: "Mental Health and Coping During COVID-19"),
             Article(image: "grat.png", title: "15 Thoughtful Quotes About Gratitude"),
-            Article(image: "nyt3.png", title: "How to Be Happy")
+            Article(image: "dep.png", title: "What is depression? - Helen M. Farrell"),
+            Article(image: "good.png", title: "Global Good News"),
+            Article(image: "Lost.png", title: "When You Feel Lost")
             
         ]
     }
